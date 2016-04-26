fb0bd94564eb08ce70cdf70d45cfd82b9c1e5887
kevintasta <yuanhan_pan@yahoo.com>
2016-04-26 00:21:07 -0400

    Initial commit

---
+++ BuzzMovieSelector/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BuzzMovieSelector.Startup))]
+namespace BuzzMovieSelector
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
